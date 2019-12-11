77bc9b47c3cd19cc8d83a405db87caa6e26094f8
alhajiry <al.hajiry16@gmail.com>
2019-12-11 17:39:39 +0700

    connect login and register to backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React, { useLayoutEffect, useState } from "react";
-import Home from "./components/Home"
+import React from "react";
+import Home from "./components/Home";
+import SignUp from "./components/RegisterForm"
+import SignIn from "./components/LoginForm"
 
-import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  
   return (
     // routing ========================================================================================================================//
     <Router>
-    <Switch>
-      <Route exact path="/">
-<Home/>
-      </Route>
-    </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <SignUp />
+        </Route>
+        <Route path="/login">
+        <SignIn/>
+        </Route>
+      </Switch>
     </Router>
-
-   
   );
 }
 
