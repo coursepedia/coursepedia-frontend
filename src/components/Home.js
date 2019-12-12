51d439a411c25b5232a9060244c6c3dcc1a28e1f
alhajiry <al.hajiry16@gmail.com>
2019-12-12 21:46:33 +0700

    change some icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -178,7 +178,7 @@ function Home() {
             <div className="col-md-4">
               <span className="fa-stack fa-4x">
                 <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                <i className="fa fa-book fa-stack-1x fa-inverse"></i>
+                <i className="fa fa-search fa-stack-1x fa-inverse"></i>
               </span>
               <h4 className="service-heading">Find Recommended Course</h4>
               <p className="text-muted">
