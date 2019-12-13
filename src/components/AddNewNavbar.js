f3ffbeb5f945f9b0e82ef8739774970c37ee7fe9
sharfinaega <ega.sharfina@gmail.com>
2019-12-13 17:17:41 +0700

    edit form

--- src/components/AddNewNavbar.js
+++ src/components/AddNewNavbar.js
@@ -1,18 +1,40 @@
 import React, { useLayoutEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+// import { makeStyles } from '@material-ui/core/styles';
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+// import EditIcon from '@material-ui/icons/Edit';
 
-function TemplateHome() {
+function AddNewNavbar() {
+  let history = useHistory();
   const [scrollY, setScrollY] = useState(0);
   const [bg, setBg] = useState("");
   const [color, setColor] = useState("");
+  const [display, setDisplay] = useState("");
+
+  const floatButtonStyle = {
+    display: "none",
+    margin: 0,
+    top: "auto",
+    right: 20,
+    bottom: 40,
+    left: "auto",
+    position: "fixed",
+    transition: "display 2s ease"
+  };
 
   function getScrollHeight() {
     setScrollY(window.pageYOffset);
-    if (scrollY >= 460) {
+    if (scrollY >= 400) {
       setBg("navdark-bg");
       setColor("textcolor-change");
+      setDisplay("floatbutton-show");
     } else {
       setBg("");
       setColor("");
+      setDisplay("");
     }
   }
 
@@ -30,9 +52,17 @@ function TemplateHome() {
     //   ('nav').toggleClass('scroll', (this).scrollTop() > 50);
     // });
   }, [getScrollHeight]);
+
   return (
     <div>
-      <nav className={`navbar navbar-expand-lg ${bg} fixed-top`} id="mainNav">
+      {/* floating action button */}
+      <div>
+        <Fab style={floatButtonStyle} className={display} color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </div>
+
+      <nav style={{ transition: "0.75s ease" }} className={`navbar navbar-expand-lg ${bg} fixed-top`} id="mainNav">
         <div className="container">
           <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <b className={`${color}`}>Coursepedia</b>
@@ -43,32 +73,40 @@ function TemplateHome() {
           </button>
           <div className="collapse navbar-collapse" id="navbarResponsive">
             <ul className="navbar-nav text-uppercase ml-auto">
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#services">
+              {/* <li className="nav-item">
+                <a className={`nav-link ${color} js-scroll-trigger`} href="#services">
                   Awesome Feature
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#portfolio">
+                <a className={`nav-link ${color} js-scroll-trigger`} href="#portfolio">
                   Courses
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#about">
+                <a className={`nav-link ${color} js-scroll-trigger`} href="#about">
                   About
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#team">
+                <a className={`nav-link ${color} js-scroll-trigger`} href="#team">
                   Testimonials
                 </a>
-              </li>
+              </li> */}
               <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#contact">
-                  Contact
+                <a className={`nav-link ${color} js-scroll-trigger`} href="#contact">
+                  Home
                 </a>
               </li>
             </ul>
+            <ButtonGroup>
+              <Button variant="outlined" color="primary" className={`nav-link ${color} js-scroll-trigger`} onClick={() => history.push("/register")}>
+                Sign Up
+              </Button>
+              <Button variant="contained" color="primary" className={`nav-link ${color} js-scroll-trigger`} onClick={() => history.push("/login")}>
+                login
+              </Button>
+            </ButtonGroup>
           </div>
         </div>
       </nav>
@@ -76,13 +114,12 @@ function TemplateHome() {
       <header className="masthead">
         <div className="container">
           <div className="intro-text">
-            <div className="intro-lead-in">Welcome To Coursepedia</div>
-            <div className="intro-heading text-uppercase">Find Recommended Courses Easily</div>
-            <a className="btn btn-primary btn-xl js-scroll-trigger" href="#services">
-              ADULTS <br /> 15 y.o
-            </a>{" "}
-            <a className="btn btn-primary btn-xl js-scroll-trigger" href="#services">
-              KIDS <br /> 8-14 y.o
+            <div className="intro-lead-norm">
+              <i>Do You Have New Recommendation Course?</i>
+            </div>
+            <div className="intro-heading text-uppercase">Please Tell To Us !</div>
+            <a className="btn btn-primary padding-sml btn-xl js-scroll-trigger" href="#form-add">
+              Get Form
             </a>
           </div>
         </div>
@@ -91,4 +128,4 @@ function TemplateHome() {
   );
 }
 
-export default TemplateHome;
+export default AddNewNavbar;
