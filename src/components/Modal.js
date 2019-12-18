2fbd48241c7a4858adf8d55fb9423b1d00727cac
dannyGan <dannyakobus@yahoo.com>
2019-12-18 17:45:50 +0700

    Fixing bug

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,10 +9,10 @@ import Rating from "@material-ui/lab/Rating";
 import CommentBox from "./CommentBox";
 
 function ModalPage() {
-  let history = useHistory();
-  const [toggle, setToggle] = useState(false);
-  const [listCourses, setListCourses] = useState([]);
-  const [modalContent, setModalContent] = useState({});
+	let history = useHistory();
+	const [toggle, setToggle] = useState(false);
+	const [listCourses, setListCourses] = useState([]);
+	const [modalContent, setModalContent] = useState({});
 
 	useEffect(() => {
 		axios.get("https://coursepediabackend.herokuapp.com/courses")
@@ -30,114 +30,105 @@ function ModalPage() {
 		setModalContent(data);
 	};
 
-  return (
-    <section className="bg-light page-section" id="portfolio">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12 text-center">
-            <h2 className="section-heading text-uppercase">
-              <b>Our Popular Courses</b>
-            </h2>
-            <h3 className="section-subheading text-muted">Find course according to your needs</h3>
-          </div>
-        </div>
-        <div className="row">
-          {listCourses.map((item, index) => {
-            if (item.name === "Impact Byte" || item.name === "Maison Bleu of Culinary Art" || item.name === "Alvin Adam Public Speaking and Communication School" || item.name === "Ohayo Drawing School" || item.name === "Anak Air Swim School" || item.name === "Engineering For Kids") {
-              return (
-                <div name="course" className="col-md-4 col-sm-6 portfolio-item">
-                  <span class="portfolio-link" data-toggle="modal">
-                    <div key={index} className="portfolio-hover" onClick={() => handleClick(item)} data-target={`$item.id`} data-toggle="modal">
-                      <div className="portfolio-hover-content">
-                        <i className="fa fa-plus fa-3x"></i>
-                      </div>
-                    </div>
-                  </span>
-                  <div className="h-50">
-                    <img src={item.imageUrl} alt="course" height="100%" width="100%" />
-                  </div>
-                  <div className="portfolio-caption">
-                    <h4>{item.name}</h4>
-                    <p className="text-muted">
-                      {new Intl.NumberFormat("id-ID", {
-                        style: "currency",
-                        currency: "idr"
-                      }).format(item.price)}
-                    </p>
-                    <br />
-                    <Box component="fieldset" mb={3} borderColor="transparent">
-                      <Rating name="read-only" value={item.rating} readOnly />
-                    </Box>
-                  </div>
-                </div>
-              );
-            }
-          })}
-        </div>
-        <div className="row">
-          <div className="col-lg-12 text-center">
-            <a className="btn btn-primary btn-xl js-scroll-trigger" onClick={() => history.push("/courses")}>
-              View More
-            </a>
-          </div>
-        </div>
-      </div>
+	return (
+		<section className="bg-light page-section" id="portfolio">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12 text-center">
+						<h2 className="section-heading text-uppercase">
+							<b>Our Popular Courses</b>
+						</h2>
+						<h3 className="section-subheading text-muted">Find course according to your needs</h3>
+					</div>
+				</div>
+				<div className="row">
+					{listCourses.map((item, index) => {
+						if (item.name === "Impact Byte" || item.name === "Maison Bleu of Culinary Art" || item.name === "Alvin Adam Public Speaking and Communication School" || item.name === "Ohayo Drawing School" || item.name === "Anak Air Swim School" || item.name === "Engineering For Kids") {
+							return (
+								<div name="course" className="col-md-4 col-sm-6 portfolio-item">
+									<span class="portfolio-link" data-toggle="modal">
+										<div key={index} className="portfolio-hover" onClick={() => handleClick(item)} data-target={`$item.id`} data-toggle="modal">
+											<div className="portfolio-hover-content">
+												<i className="fa fa-plus fa-3x"></i>
+											</div>
+										</div>
+									</span>
+									<div className="h-50">
+										<img src={item.imageUrl} alt="course" height="100%" width="100%" />
+									</div>
+									<div className="portfolio-caption">
+										<h4>{item.name}</h4>
+										<p className="text-muted">
+											{new Intl.NumberFormat("id-ID", {
+												style: "currency",
+												currency: "idr"
+											}).format(item.price)}
+										</p>
+										<br />
+										<Box component="fieldset" mb={3} borderColor="transparent">
+											<Rating name="read-only" value={item.rating} readOnly />
+										</Box>
+									</div>
+								</div>
+							);
+						}
+					})}
+				</div>
+				<div className="row">
+					<div className="col-lg-12 text-center">
+						<a className="btn btn-primary btn-xl js-scroll-trigger" onClick={() => history.push("/courses")}>
+							View More
+						</a>
+					</div>
+				</div>
+			</div>
 
-      {/* Modal */}
-      {Object.keys(modalContent).length !== 0 && (
-        <MDBContainer name="modal">
-          <MDBModal isOpen={toggle} size="lg" centered>
-            <MDBModalBody>
-              <>
-                <MDBModalHeader class="text-uppercase">{modalContent.name}</MDBModalHeader>
-                <img className="img-fluid d-block mx-auto" src={modalContent.imageUrl} alt="" />
-              </>
-              <div className="portofolio-caption">
-                <ul class="list-inline">
-                  <li>
-                    <h4 className="orange-text">
-                      {" "}
-                      <Rating name="read-only" value={modalContent.rating} readOnly /> {modalContent.rating}
-                    </h4>
-                  </li>
-                  <li className="pb-2">
-                    <strong>Address: </strong>
-                    {modalContent.address}
-                  </li>
-                  <li className="pb-2">
-                    <strong>Telephone: </strong>
-                    {modalContent.phoneNumber}
-                  </li>
-                  <li className="pb-2">
-                    <strong>Age: </strong>
-                    <span className="text-capitalize">{modalContent.ageCategory}</span>
-                  </li>
-                  <li className="pb-2">
-                    <strong>Price : </strong>
-                    {new Intl.NumberFormat("id-ID", {
-                      style: "currency",
-                      currency: "idr"
-                    }).format(modalContent.price)}
-                  </li>
-                  <li className="pb-2">
-                    <strong>Type : </strong>
-                    <span className="text-capitalize">{modalContent.fieldCategory}</span>
-                  </li>
-                  {/* <li className="pb-2">
-                    <div className="input-group">
-                      <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon">
-                          <i class="fa fa-pencil" aria-hidden="true"></i>
-                        </span>
-                      </div>
-                      <textarea
-                        className="form-control"
-                        id="exampleFormControlTextarea1"
-                        rows="5"
-                      ></textarea>
-                    </div>
-                     {modalContent.comments[0].users.username}
-                    {modalContent.comments[0].content}
+			{/* Modal */}
+			{Object.keys(modalContent).length !== 0 && (
+				<MDBContainer name="modal">
+					<MDBModal isOpen={toggle} size="lg" centered>
+						<MDBModalBody>
+							<>
+								<MDBModalHeader class="text-uppercase">{modalContent.name}</MDBModalHeader>
+								<img className="img-fluid d-block mx-auto" src={modalContent.imageUrl} alt="" />
+							</>
+							<div className="portofolio-caption">
+								<ul class="list-inline">
+									<li>
+										<h4 className="orange-text">
+											{" "}
+											<Rating name="read-only" value={modalContent.rating} readOnly /> {modalContent.rating}
+										</h4>
+									</li>
+									<li className="pb-2">
+										<strong>Address: </strong>
+										{modalContent.address}
+									</li>
+									<li className="pb-2">
+										<strong>Telephone: </strong>
+										{modalContent.phoneNumber}
+									</li>
+									<li className="pb-2">
+										<strong>Age: </strong>
+										<span className="text-capitalize">{modalContent.ageCategory}</span>
+									</li>
+									<li className="pb-2">
+										<strong>Price : </strong>
+										{new Intl.NumberFormat("id-ID", {
+											style: "currency",
+											currency: "idr"
+										}).format(modalContent.price)}
+									</li>
+									<li className="pb-2">
+										<strong>Type : </strong>
+										<span className="text-capitalize">{modalContent.fieldCategory}</span>
+									</li>
+								</ul>
+							</div>
+						</MDBModalBody>
+					</MDBModal>
+				</MDBContainer>
+			)}
 
 			{/* Modal */}
 			{Object.keys(modalContent).length !== 0 && (
